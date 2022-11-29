4f4f0854f44c711708f08511b6cab1435690c711
@uqpyx <uqpyx@student.kit.edu>
2022-11-29 11:04:54 +0100

    Add middleware

---
+++ helpers/middleware/parseJwt.js
@@ -0,0 +1,19 @@
+const atob = require("atob");
+
+//@desc this function is for parsing the jwt token and extracting the payload
+function parseJwt(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
+module.exports = parseJwt;
---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const parseJwt = require("../helpers/middleware/parseJwt");
+
+//@desc middleware responsible for token verification ( Private methods ) (Authorization)
+module.exports = function (req, res, next) {
+  // Get token from header ( when we send a request to a prootected route we need this middleware)
+  const token = req.header("x-auth-token");
+
+  // Check the existence of the token
+  if (!token) {
+    return res.status(401).json({ message: "No token found!" });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    const decodePayload = parseJwt(token);
+
+    req.user = decoded.user;
+    next();
+  } catch (e) {
+    res.status(401).json({ message: "Token is invalid" });
+  }
+};
